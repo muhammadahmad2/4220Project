f9de8c1e5f52e25577df44e65acde6c4f662adbe
muhammadahmad2 <muhammad.ahmad2@uoit.net>
2018-04-08 15:21:50 -0400

    Enabled webcam

--- Project.py
+++ Project.py
@@ -1,20 +1,23 @@
-
 import os
 import cv2
 import sys
 import random
 import numpy as np
 
+
+def draw_rects(img, x1, y1, x2, y2, color):
+    cv2.rectangle(img, (x1, y1), (x2, y2), color, 2)
+
 class Eigenfaces(object):                                                       
-    l = 400                                      
+    images = 400                                      
     m = 92                                                                      
     n = 112                                                                     
     mn = m * n                                                                  
     
-    def __init__(self, _energy = 0.85):
-        self.energy = _energy
+    def __init__(self, limit = 0.85):
+        self.limit = limit
         
-        L = np.empty(shape=(self.mn, self.l), dtype='float64')
+        L = np.empty(shape=(self.mn, self.images), dtype='float64')
 
         i = 0
         for imgfolder in os.listdir('./Bioid/'):
@@ -29,37 +32,37 @@ class Eigenfaces(object):
                     L[:, i] = img_col[:]                              
                     i += 1                                           
 
-        self.mean_img_col = np.sum(L, axis=1) / self.l            
+        self.mean_img = np.sum(L, axis=1) / self.images            
         
-        for j in range(0, self.l):                                
-            L[:, j] -= self.mean_img_col[:]
+        for j in range(0, self.images):                                
+            L[:, j] -= self.mean_img[:]
 
-        C = np.matrix(L.transpose()) * np.matrix(L)      
-        C /= self.l                                                             
+        covariance = np.matrix(L.transpose()) * np.matrix(L)      
+        covariance /= self.images                                                             
 
-        self.evalues, self.evectors = np.linalg.eig(C)                          
-        sort_indices = self.evalues.argsort()[::-1]                             
-        self.evalues = self.evalues[sort_indices]                               
-        self.evectors = self.evectors[sort_indices]                             
+        eigenvalues, eigenvectors = np.linalg.eig(covariance)                          
+        sort = eigenvalues.argsort()[::-1]                             
+        eigenvalues = eigenvalues[sort]                               
+        eigenvectors = eigenvectors[sort]                             
         
-        evalues_sum = sum(self.evalues[:])                                      
-        evalues_count = 0                                                       
-        evalues_energy = 0.0
-        for evalue in self.evalues:
-            evalues_count += 1
-            evalues_energy += evalue / evalues_sum
+        eigenvalues_sum = sum(eigenvalues[:])                                      
+        eigenvalues_count = 0                                                       
+        eigenvalues_limit = 0.0
+        for eigenvalue in eigenvalues:
+            eigenvalues_count += 1
+            eigenvalues_limit += eigenvalue / eigenvalues_sum
 
-            if evalues_energy >= self.energy:
+            if eigenvalues_limit >= self.limit:
                 break
 
-        self.evalues = self.evalues[0:evalues_count]                            
-        self.evectors = self.evectors[0:evalues_count]
+        eigenvalues = eigenvalues[0:eigenvalues_count]                            
+        eigenvectors = eigenvectors[0:eigenvalues_count]
 
-        self.evectors = self.evectors.transpose()                               
-        self.evectors = L * self.evectors                                       
-        norms = np.linalg.norm(self.evectors, axis=0)                          
-        self.evectors = self.evectors / norms  
-        self.W = self.evectors.transpose() * L
+        eigenvectors = eigenvectors.transpose()                               
+        eigenvectors = L * eigenvectors                                       
+        norms = np.linalg.norm(eigenvectors, axis=0)                          
+        self.eigenvectors = eigenvectors / norms  
+        self.W = self.eigenvectors.transpose() * L
         
     def classify(self, img):
 
@@ -68,14 +71,13 @@ class Eigenfaces(object):
 
         
         img = cv2.resize(img,(92, 112), interpolation = cv2.INTER_AREA)   
-        img_col = np.array(img, dtype='float64').flatten()  
-        img_col1=img_col
-        img_col -= self.mean_img_col                                         
-        img_col2 = img_col
-        img_col = np.reshape(img_col, (self.mn, 1))                         
-        img_col3 =img_col
+        imgs = np.array(img, dtype='float64').flatten()  
+
         
-        S = self.evectors.transpose() * img_col                                
+        imgs -= self.mean_img
+        imgs = np.reshape(imgs, (self.mn, 1))
+        
+        S = self.eigenvectors.transpose() * imgs                               
         
         diff = self.W - S                                                       
         
@@ -83,15 +85,14 @@ class Eigenfaces(object):
 
         liv = np.argmin(norms)                                     
         return liv,norms
-    
-def draw_rects(img, x1, y1, x2, y2, color):
-    cv2.rectangle(img, (x1, y1), (x2, y2), color, 2)
-    
-    
+
+
 if __name__ == "__main__":
     efaces = Eigenfaces()
     
-    cam = cv2.VideoCapture('./video.mpg')
+    capture = 0
+    
+    cam = cv2.VideoCapture(0)
     images = []
     runtime = 0
     height1, width1, channels1 = 0,0,0
@@ -100,60 +101,73 @@ if __name__ == "__main__":
     
     while True:
         ret,img = cam.read()
-        runtime +=1
+        img = cv2.flip(img, 1)
+        
         
         if ret == False:
             break
+        if(runtime%2==0):
+            
+            height1, width1, channels1 = img.shape
 
-        height1, width1, channels1 = img.shape
-        
-        div = int(width1/100)
-    
-        if(div == 0):
-            div = 1
+            div = int(width1/150)
 
-        vis = cv2.resize(img,(int(width1/div), int(height1/div)), interpolation = cv2.INTER_AREA)
-        height, width, channels = vis.shape
+            if(div == 0):
+                div = 1
 
-        x = 92
-        y = 112
+            vis = cv2.resize(img,(int(width1/div), int(height1/div)), interpolation = cv2.INTER_AREA)
+            height, width, channels = vis.shape
 
-        nom=[]
-        xy = []
-        for i in range(4):
-            w = 0
-            for a in range(width):
-                h = 0
-                if((w + x)>width):
-                    break
-                for b in range(height):
-                    if((h + y)>height):
+            x = 46
+            y = 56
+
+            nom=[]
+            xy = []
+            for i in range(1):
+                w = 0
+                for a in range(width):
+                    h = 0
+                    if((w + x)>width):
                         break
-                    temp = vis[h : h + y, w : w + x]
-                    liv,norms = efaces.classify(temp)
-                    nom.append(norms[liv])
-                    xy.append([h*div,y*div,w*div,x*div])
-                    h += 5
+                    for b in range(height):
+                        if((h + y)>height):
+                            break
+                        temp = vis[h : h + y, w : w + x]
+                        liv,norms = efaces.classify(temp)
+                        nom.append(norms[liv])
+                        xy.append([h*div,y*div,w*div,x*div])
 
-                w += 5
-            x -= 20
-            y -= 24
-                
-        low = np.argmin(nom)
-        h,y,w,x=xy[low]
-        if(nom[low]<=3800):     
-            draw_rects(img, w,h,w + x,h + y, (255,0,0))
+                        h += 5
+
+                    w += 5
+                x -= 10
+                y -= 12
+
+            low = np.argmin(nom)
+            h,y,w,x=xy[low]
         
+        runtime +=1
+        if(nom[low]<=4000):  
+            draw_rects(img, w,h,w + x-5,h + y-5, (255,0,0))
+
         images.append(img)
+        
+        cv2.imshow('cam',img)
+        if cv2.waitKey(1) == 27: 
+            break
+    cv2.destroyAllWindows()
         
     done = time.time()
     elapsed = done - start
     print(elapsed)
     
-        
-    out = cv2.VideoWriter('output.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 25, (width1,height1))
+    cam.release()
+    
+    if(capture==0):
+        out = cv2.VideoWriter('output2.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 25, (width1,height1))
 
-    for image in images:
-        out.write(image)
+        for image in images:
+            out.write(image)
 
-    out.release()
+        out.release()
+
